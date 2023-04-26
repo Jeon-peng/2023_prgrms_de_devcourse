2cdbebf7aa9d4ae61b942fd6e0f58b9dd5cbd2ef
Jeon-peng <pengping@kakao.com>
2023-04-26 13:58:18 +0900

    0526_GET
    polls_api 폴더 추가
    http://127.0.0.1:8000/rest/question/ 접속 시 Question List 나타남

--- 3weeks/mysite/mysite/urls.py
+++ 3weeks/mysite/mysite/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    path('', include('polls.urls'))
-    # path('some_page/', include('polls.urls')),
+    path('rest/', include('polls_api.urls')),
 ]
