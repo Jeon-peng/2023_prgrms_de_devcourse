26210ce0f8ffc0c995dbee735ec0d079e0177eba
Jeon-peng <pengping@kakao.com>
2023-04-26 21:02:38 +0900

    Class Views
    path를 QuestionList.as_view()로 변환
    
    views.py 에서 Class - QuestionList 생성

--- 3weeks/mysite/polls_api/views.py
+++ 3weeks/mysite/polls_api/views.py
@@ -4,16 +4,16 @@ from polls_api.serializers import QuestionSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.views import APIView
 
-
-@api_view(['GET','POST'])
-def question_list(request):
-    if request.method == 'GET' :
+class QuestionList(APIView):
+    def get(self, request):
         questions = Question.objects.all()
         serializer = QuestionSerializer(questions, many = True)
         return Response(serializer.data)
-    
-    if request.method == 'POST' :
+        
+        
+    def post(self, request):
         serializer = QuestionSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
@@ -22,24 +22,61 @@ def question_list(request):
             return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
         
         
-@api_view(['GET','PUT', 'DELETE'])
-def question_detail(request, id):
-    question = get_object_or_404(Question,pk = id)
-    
-    if request.method == 'GET' :
+class QuestionDetail(APIView):
+    def get(self, request, id):
+        question = get_object_or_404(Question,pk = id)
         serializer = QuestionSerializer(question)
         return Response(serializer.data)
-    
-    if request.method == 'PUT' :
-        serializer = QuestionSerializer(question, data = request.data)
         
+    def put(self, request, id):
+        question = get_object_or_404(Question,pk = id)
+        serializer = QuestionSerializer(question, data = request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status = status.HTTP_200_OK)
         
         else :
             return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-    
-    if request.method == 'DELETE' :
+        
+    def delete(self, request, id):
+        question = get_object_or_404(Question,pk = id)
         question.delete()
         return Response(status = status.HTTP_204_NO_CONTENT)
+
+# @api_view(['GET','POST'])
+# def question_list(request):
+#     if request.method == 'GET' :
+#         questions = Question.objects.all()
+#         serializer = QuestionSerializer(questions, many = True)
+#         return Response(serializer.data)
+    
+#     if request.method == 'POST' :
+#         serializer = QuestionSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status = status.HTTP_201_CREATED)
+#         else :
+#             return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        
+        
+# @api_view(['GET','PUT', 'DELETE'])
+# def question_detail(request, id):
+#     question = get_object_or_404(Question,pk = id)
+    
+#     if request.method == 'GET' :
+#         serializer = QuestionSerializer(question)
+#         return Response(serializer.data)
+    
+#     if request.method == 'PUT' :
+#         serializer = QuestionSerializer(question, data = request.data)
+        
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status = status.HTTP_200_OK)
+        
+#         else :
+#             return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+#     if request.method == 'DELETE' :
+#         question.delete()
+#         return Response(status = status.HTTP_204_NO_CONTENT)
