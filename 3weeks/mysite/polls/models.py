0b5315608780b60350237db1e8e879f8a448d1f9
Jeon-peng <pengping@kakao.com>
2023-05-02 23:21:42 +0900

    update

--- 3weeks/mysite/polls/models.py
+++ 3weeks/mysite/polls/models.py
@@ -7,33 +7,17 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     question_text = models.CharField(max_length=200, verbose_name = '질문')
     pub_data = models.DateTimeField(auto_now_add = True, verbose_name='생성일')
-    # score = models.FloatField(default = 0)
-    # is_something_wrong = models.BooleanField(default = False)
-    # json_field = models.JSONField(default=dict)
-    owner = models.ForeignKey('auth.User',related_name = 'questions',on_delete=models.CASCADE, null = True)
+    # owner = models.ForeignKey('auth.User',related_name = 'questions',on_delete=models.CASCADE, null = True)
     
-    
-    @admin.display(boolean=True, description='최근생성(하루기준)')
-    def was_published_recently(self):
-        return self.pub_data >= timezone.now() - datetime.timedelta(days=1)
-    
-    def __str__(self):
-        if self.was_published_recently :
-            new_badge = 'NEW!!!'
-        else :
-            new_badge = ''
-        return f'{new_badge}제목 : {self.question_text}, 날짜: {self.pub_data}'
-    
-    
+    # SQLite3 에는 있는데 Django에서는 안보이는건지???
+    # 데베 자체에서 제거해버리겠다는건지
     
 class Choice(models.Model):
     question = models.ForeignKey(Question, related_name='choices', on_delete = models.CASCADE)
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default = 0)
-    
     def __str__(self):
         return f'[{self.question.question_text}] {self.choice_text}'
-    
     
     
 class Vote(models.Model):
